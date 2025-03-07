a1b916cd967f03ae23d7c03fc1c2583b00466e00
senai_24_25 <wswomans.secrets@gmail.com>
2025-03-07 10:02:08 -0300

    Initial commit

---
+++ exemplo.js
@@ -0,0 +1,28 @@
+// alert('Olá Mundo')
+//
+// var test = prompt('Digite qualquer coisa')
+//
+// alert(test)
+
+function testtexto() {
+    document.getElementById("titulo").innerHTML = "Boa Tarde"
+}
+
+function testcolor() {
+    const titulo = document.getElementById("titulo")
+
+    titulo.style.color = 'red'
+    titulo.style.fontSize = '50px'
+}
+
+function reset() {
+    document.getElementById("titulo").innerHTML = "Celebrate the petals & the prickles this Mother's Day"
+}
+
+function testinput() {
+    document.getElementById("input").value = "Olá"
+}
+
+
+
+
